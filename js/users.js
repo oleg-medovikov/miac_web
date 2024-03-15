422675033c76082cc5bef8f1e082563893f2b3fd
OLKHVSKIY <s.olkhovskiy04@gmail.com>
2024-03-15 14:48:00 +0300

    finished the table, added the user

--- js/users.js
+++ js/users.js
@@ -1,12 +1,64 @@
 import config from '../config.js';
-    // Отправляем запрос к API
+  
+// Проверяем наличие токена в localStorage
+if (!localStorage.getItem('authToken')) {
+  // Если токена нет, перенаправляем на страницу входа
+  window.location.href = '../html/login.html';
+} else {
+  // Отправляем запрос к API для проверки токена
+  fetch(`${config.ApiUrl}/check_token`, {
+    method: 'GET',
+    headers: {
+      'Authorization': localStorage.getItem('authToken')
+    }
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    if (!data.token_valid) {
+      // Если токен не валиден, перенаправляем на страницу входа
+      window.location.href = '../html/login.html';
+    } else { // Если токен валидный - мы вытаскиваем пользователя
+      fetch(`${config.ApiUrl}/user_get`, { 
+        method: 'GET',
+        headers: {
+          'Authorization': localStorage.getItem('authToken')
+        }
+      })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json(); // Преобразуем тело ответа в JSON
+      })
+      .then(data => {  
+        localStorage.setItem('fio', data.fio); 
+        localStorage.setItem('groups', data.groups);
+        document.getElementById('UserFIO').innerHTML = localStorage.getItem('fio');
+        document.getElementById('UserGroups').innerHTML = localStorage.getItem('groups');
+      })
+      .catch(error => {
+        console.error('There has been a problem with your fetch operation:', error);
+      });
+    }
+  })
+  .catch(error => {
+    console.error('There was a problem with the fetch operation:', error);
+  });
+}
+
+// Отправляем запрос к API
     fetch(`${config.ApiUrl}/user_get_all`, {
       method: 'GET',
       headers: {
         'Authorization': localStorage.getItem('authToken')
       }
     })
-      .then(response => response.json())
+      .then(response => response.json()) 
       .then(data => {
         // Создаем таблицу пользователей
         const usersTable = document.createElement('table');
@@ -69,14 +121,28 @@ function openEditUserModal(user) {
   // Получаем модальное окно
   modal = document.getElementById('editUserModal');
 
+// Проверяем, существует ли user
+if (user) {
   // Заполняем поля формы данными пользователя
-  document.getElementById('guid').value = user.guid;
-  document.getElementById('tg_id').value = user.tg_id;
-  document.getElementById('username').value = user.username;
-  document.getElementById('fio').value = user.fio;
-  document.getElementById('groups').value = user.groups;
-  document.getElementById('description').value = user.description;
+  document.getElementById('guid').value = user.guid || '';
+  document.getElementById('tg_id').value = user.tg_id || '';
+  document.getElementById('username').value = user.username || '';
+  document.getElementById('fio').value = user.fio || '';
+  document.getElementById('groups').value = user.groups || '';
+  document.getElementById('description').value = user.description || '';
   document.getElementById('active').value = user.active ? 'true' : 'false';
+  document.getElementById('updateModalButton').style.display = 'block';
+} else {
+  // Если user не определен, очищаем поля формы
+  document.getElementById('guid').value = '';
+  document.getElementById('tg_id').value = '';
+  document.getElementById('username').value = '';
+  document.getElementById('fio').value = '';
+  document.getElementById('groups').value = '';
+  document.getElementById('description').value = '';
+  document.getElementById('active').value = ''; 
+  document.getElementById('createModalButton').style.display = 'block';
+}
 
   // Отображаем модальное окно
   modal.style.display = 'block';
@@ -90,7 +156,7 @@ closeBtn.addEventListener("click", function() {
   modal.style.display = "none";
 });
 
-var okBtn = document.getElementById("okModalButton");
+var okBtn = document.getElementById("updateModalButton");
 okBtn.addEventListener("click", function() {
  // Получаем данные из формы
   var guid = document.getElementById('guid').value;
@@ -125,7 +191,60 @@ okBtn.addEventListener("click", function() {
   .then(data => {
     console.log('Успешно:', data);
     // Здесь можно обработать ответ от сервера, например, обновить интерфейс пользователя
-    modal.style.display = "none";
+    modal.style.display = "none"; 
+    location.reload();
   });
 });
   
+// Получаем кнопку открытия для создания пользователя
+var addBtn = document.getElementById("addUserModal");
+  // Добавляем обработчик события на кнопку закрытия
+addBtn.addEventListener("click", function() {
+  openEditUserModal();
+});
+
+//создание нового пользователя
+
+var okBtn = document.getElementById("createModalButton");
+okBtn.addEventListener("click", function() {
+ // Получаем данные из формы
+  var tg_id = document.getElementById('tg_id').value;
+  var username = document.getElementById('username').value;
+  var fio = document.getElementById('fio').value;
+  var groups = document.getElementById('groups').value;
+  var description = document.getElementById('description').value;
+  var active = document.getElementById('active').value === 'true';
+
+   // Проверяем, заполнены ли все поля
+   if (!tg_id || !username || !fio || !groups || !description) {
+    alert('Все поля должны быть заполнены.');
+    return; // Прекращаем выполнение функции, если какое-либо поле не заполнено
+  }
+
+  // Создаем объект с данными
+  var data = {
+    tg_id: parseInt(tg_id),
+    username: username,
+    fio: fio,
+    groups: groups,
+    description: description,
+    active: active
+  };
+
+  // Отправляем запрос
+  fetch(`${config.ApiUrl}/user_create`, {
+    method: 'POST',
+    headers: {
+      'Content-Type':'application/json',
+      'Authorization': localStorage.getItem('authToken')
+    },
+    body: JSON.stringify(data)
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log('Успешно:', data);
+    // Здесь можно обработать ответ от сервера, например, обновить интерфейс пользователя
+    modal.style.display = "none"; 
+    location.reload();
+  });
+});
